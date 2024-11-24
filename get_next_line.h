0027bd2324fab7f9934af6a9361f7591c4dcf40e
Abdelali Ouaalla <aouaalla@e2r9p2.1337.ma>
2024-11-24 17:44:26 +0100

    modified get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 4
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 # include <stdio.h> // temporarly
 
-char	get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
