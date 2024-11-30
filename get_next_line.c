fe6606f510ae0499e8c99e2f2fdfd9e00c1d07e5
Abdelali Ouaalla <aouaalla@e2r3p14.1337.ma>
2024-11-30 17:44:13 +0100

    get_next_line.h

--- get_next_line.c
+++ get_next_line.c
@@ -11,52 +11,103 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-char	*init(int fd, char *s, char *buffer)
+static void	clear(char *p)
+{
+	free(p);
+	p = NULL;
+}
+
+static char	*init(int fd, char *s, char *buffer)
 {
 	char	*tmp;
-	ssize_t	i;
+	size_t	i;
 
-	while ((i = read(fd, buffer, BUFFER_SIZE)))
+	i = 1;
+	while (i)
 	{
+		i = read(fd, buffer, BUFFER_SIZE);
 		if (i < 0)
 		{
-			free(buffer);
-			buffer = NULL;
+			clear(buffer);
 			return (buffer);
 		}
 		buffer[i] = '\0';
 		tmp = s;
 		s = ft_strjoin(tmp, buffer);
+		clear(tmp);
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
 	return (s);
 }
 
-void	clear(char *p)
+static char	*before_nline(char *str)
 {
-	free(p);
-	p = NULL;
+	char	*p;
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	while (str[len] && str[len] != 10)
+		len++;
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (p);
+	i = 0;
+	while (i < len)
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = 0;
+	return (p);
+}
+
+static char	*after_nline(char *str)
+{
+	char	*p;
+	size_t	len;
+	size_t	i;
+
+	while (*str != 10 && *str)
+		str++;
+	str++;
+	len = 0;
+	while (str[len])
+		len++;
+	if (!len)
+		return (ft_strdup(""));
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (p);
+	i = 0;
+	while (i < len)
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = 0;
+	return (p);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*st;
 	char		*p;
+	char		*container;
 	char		*line;
 
 	p = NULL;
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= INT_MAX)
 		return (clear(st), st);
 	p = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!p)
 		return (0);
-	line = init(fd, st, p);
+	container = init(fd, st, p);
+	st = after_nline(container);
+	line = before_nline(container);
 	clear(p);
+	clear(container);
 	return (line);
 }
-
-// code substr
-// think about a way to copy whats after the \n and merge it with the new line (process)
