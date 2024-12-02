611edb34d7f00dad9a9eb8b0e293e6457a9ed1e9
Abdelali Ouaalla <aouaalla@e2r7p3.1337.ma>
2024-12-02 15:09:37 +0100

    modified get_next_line.c
    fixed empty line read, it should return null instead of an empty string when reaching the end of file. (the function still has some leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,13 @@
 /*   By: aouaalla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 14:04:39 by aouaalla          #+#    #+#             */
-/*   Updated: 2024/11/28 14:04:41 by aouaalla         ###   ########.fr       */
+/*   Updated: 2024/12/02 15:08:02 by aouaalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <errno.h>
 
 static void	clear(char *p)
 {
@@ -48,8 +49,14 @@ static char	*before_nline(char *str)
 	size_t	len;
 
 	len = 0;
+	if (*str == 10)
+		return (ft_substr(str, 0, 1));
 	while (str[len] && str[len] != 10)
 		len++;
+	if (str[len] == 10)
+		len++;
+	if (len == 0)
+		return (0);
 	return (ft_substr(str, 0, len));
 }
 
@@ -78,7 +85,7 @@ char	*get_next_line(int fd)
 		return (clear(st), st);
 	p = (char *)malloc(((size_t)(BUFFER_SIZE + 1)) * sizeof(char));
 	if (!p)
-		return (0);
+		return (p);
 	container = init(fd, st, p);
 	st = after_nline(container);
 	line = before_nline(container);
@@ -86,3 +93,4 @@ char	*get_next_line(int fd)
 	clear(container);
 	return (line);
 }
+
